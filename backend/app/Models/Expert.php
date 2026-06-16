198c82ce4fc835650c897fa03dd4372e90df249f
amirghasempoor <amirghasempoor79@gmail.com>
2026-06-16 14:07:33 +0330

    fix the middleware

--- backend/app/Models/Expert.php
+++ backend/app/Models/Expert.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Database\Factories\ExpertFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class Expert extends Model
+class Expert extends Authenticatable
 {
     /** @use HasFactory<ExpertFactory> */
     use HasFactory, HasRoles;
